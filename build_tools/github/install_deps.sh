c387d712e449c566d66d7145b95d48c8c010ffcf
Tom M. Ragonneau <tom.ragonneau@connect.polyu.hk>
2022-03-16 10:48:38 +0800

    Install virtual environment

--- build_tools/github/install_deps.sh
+++ build_tools/github/install_deps.sh
@@ -3,6 +3,8 @@
 set -e
 set -x
 
+python -m venv venv
+
 if [[ "$RUNNER_OS" == "Linux" ]]; then
     sudo apt-get update
     sudo apt-get install texlive-full --fix-missing
@@ -14,5 +16,6 @@ else
     exit 1
 fi
 
+source venv/bin/activate
 python -m pip install -r requirements.txt
 python -m pip install -i https://pypi.anaconda.org/ragonneau/simple cobyqa
--- build_tools/github/run_profiles.sh
+++ build_tools/github/run_profiles.sh
@@ -3,5 +3,6 @@
 set -e
 set -x
 
+source venv/bin/activate
 python main.py
 find archives -type f -name "*.csv" -exec rm -rf {} \;
